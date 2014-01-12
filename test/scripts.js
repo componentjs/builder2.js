c972c8b39db0ea0bfaa7522a3ae731c9256acf0c
Jonathan Ong <jonathanrichardong@gmail.com>
2014-01-12 07:33:31 -0800

    fix template and json support

--- test/scripts.js
+++ test/scripts.js
@@ -82,3 +82,51 @@ describe('js-main', function () {
     vm.runInContext('if (!this.two) throw new Error()', ctx)
   })
 })
+
+describe('js-json', function () {
+  var tree
+  var nodes
+  var js
+
+  it('should install', co(function* () {
+    var resolver = new Resolver(fixture('js-json'), options)
+    tree = yield* resolver.tree()
+    nodes = resolver.flatten(tree)
+  }))
+
+  it('should build', co(function* () {
+    var builder = new Builder(nodes)
+    js = yield builder.toStr()
+  }))
+
+  it('should execute', function () {
+    var ctx = vm.createContext()
+    vm.runInContext(js, ctx)
+    vm.runInContext('var json = require("js-json")', ctx)
+    vm.runInContext('if (json.message !== "hello") throw new Error()', ctx)
+  })
+})
+
+describe('js-templates', function () {
+  var tree
+  var nodes
+  var js
+
+  it('should install', co(function* () {
+    var resolver = new Resolver(fixture('js-templates'), options)
+    tree = yield* resolver.tree()
+    nodes = resolver.flatten(tree)
+  }))
+
+  it('should build', co(function* () {
+    var builder = new Builder(nodes)
+    js = yield builder.toStr()
+  }))
+
+  it('should execute', function () {
+    var ctx = vm.createContext()
+    vm.runInContext(js, ctx)
+    vm.runInContext('var string = require("js-templates")', ctx)
+    vm.runInContext('if (string !== "<p>hi</p>") throw new Error()', ctx)
+  })
+})
